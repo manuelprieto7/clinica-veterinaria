b4cc1ecc338312cedd3ca7f5b4822f8bb49b69cc
Manuelprieto7 <mprieto.1996@gmail.com>
2026-04-05 14:36:43 +0200

    feat: agregar codigo fuente inicial del proyecto Peluqueria Canina

---
+++ src/main/java/prieto/manuel/maven/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prieto.manuel.maven.peluqueriacanina;
+
+/**
+ *
+ * @author G513
+ */
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
